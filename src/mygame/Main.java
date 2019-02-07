501c45ecb6441da7128cc194273dd49d89974d5e
larald15 <larald15@htlkaindorf.at>
2019-02-07 10:27:12 +0100

    Initial Commit

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,16 +1,16 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
  * Move your Logic into AppStates or Controls
+ *
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
@@ -22,12 +22,7 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Spatial mug = assetManager.loadModel("Models/Mug-red/mug-red.j3o");
-        Material test = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        test.setTexture("ColorMap", assetManager.loadTexture("Models/Mug-red/Mug-red.png"));
-        
-        mug.setMaterial(test);
-        rootNode.attachChild(mug);
+        Spatial test = getAssetManager()
     }
 
     @Override
