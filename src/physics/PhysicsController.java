a56c38a03fdbfb780dcd81fe3b1b1435e1b6d984
larald15 <larald15@htlkaindorf.at>
2019-02-07 11:34:34 +0100

    Map + Collisions

---
+++ src/physics/PhysicsController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physics;
+
+/**
+ *
+ * @author Larsen
+ */
+public class GamePhysics {
+    
+    //Set up the basic Physics
+    public void initPhysics() {
+        
+    }
+    
+}
